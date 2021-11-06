6b1df0790215df3672e5e55e8a77afbe06d3a124
camsn0w <camhconnell@gmail.com>
2021-11-06 17:40:19 -0600

    Removed unneeded parameter

--- service.go
+++ service.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func mainHandler(w http.ResponseWriter, r *http.Request) {
+func mainHandler(w http.ResponseWriter, _ *http.Request) {
 	name, err := getName()
 	if err != nil {
 		log.Panicln(err)
