7e3b61a88a79bf4c2667b2efcdba448c270a7ceb
camsn0w <camhconnell@gmail.com>
2022-05-06 20:36:31 -0600

    Switched to gin

--- service.go
+++ service.go
@@ -1,62 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
+	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-// mainHandler() responds to requests to /* with the joke.
-func mainHandler(w http.ResponseWriter, _ *http.Request) {
-	name, err := getName()
-	if err != nil {
-		log.Panicln(err)
-	}
-	joke, err := name.getJoke()
-	if err != nil {
-		log.Panicln(err)
-	}
-	fmt.Fprintf(w, joke.Value.Joke)
-}
-
-// getName() calls the name api, returning a Name and error.
-func getName() (Name, error) {
-	res, err := http.Get(nameUrl)
-	if err != nil {
-		return Name{}, err
-	}
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return Name{}, err
-	}
-	return bytesToName(body)
-}
-
-// getJoke() gets the joke for a given Name.
-func (name *Name) getJoke() (Joke, error) {
-	jokeUrl := name.getJokeUrl()
-	res, err := http.Get(jokeUrl)
-	if err != nil {
-		return Joke{}, err
-	}
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return Joke{}, err
-	}
-	return bytesToJoke(body)
-}
-
-// TODOs for full production ready:
-// Depending on the intended response for a rate limit, possibly
-// respond with a result from an archived joke + name
-
-// startServer() specifies the handle functions and port and starts the server.
-func startServer() {
-	http.HandleFunc("/", mainHandler)
-	log.Panicln(http.ListenAndServe(":5000", nil))
+func getJoke(c *gin.Context) {
+	got := NewJoke()
+	c.IndentedJSON(http.StatusOK, got)
 }
 
 func main() {
-	startServer()
+	router := gin.Default()
+	router.GET("/", getJoke)
+	router.Run(":8080")
 }
