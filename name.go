0037ac7a9d3a0c11c62360b6d26520aca549c005
camsn0w <camhconnell@gmail.com>
2022-05-06 20:37:04 -0600

    Simplified name.go

--- name.go
+++ name.go
@@ -1,25 +1,12 @@
 package main
 
-import "encoding/json"
-
 const nameUrl = "https://names.mcquay.me/api/v0/"
 
-type Name struct {
+type name struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 }
 
-// getJokeUrl() makes the url for the joke request
-// using the given Name.
-func (name *Name) getJokeUrl() string {
-	return "http://api.icndb.com/jokes/random?firstName=" + name.FirstName + "&lastName=" + name.LastName + "&limitTo=nerdy"
-}
-
-// TODO: add input validation for cases where it doesn't match the expected json.
-// BytesToName() takes bytes from a request and returns
-// a Name and error.
-func bytesToName(rawName []byte) (Name, error) {
-	name := Name{}
-	err := json.Unmarshal(rawName, &name)
-	return name, err
+func (n *name) GetUrl() string {
+	return nameUrl
 }
